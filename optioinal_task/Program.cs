948e2f2956c81bb9e7861a17f52d9bac2f8e062d
Artem <dvinskikh_artem@mail.ru>
2023-06-10 10:31:31 +0900

    solution task

---
+++ optioinal_task/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите количество программистов: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int num1=num%10;
+int num2=num%100;
+if (num1==1&num2!=11)
+{
+    System.Console.WriteLine("Программист");
+}
+else if (num1==5||num2==11||num2==12||num2==13||num2==14)
+    System.Console.WriteLine("Программистов");
+else if (num1==2||num1==3||num1==4)
+    System.Console.WriteLine("Программиста");
+else if (num1>5)
+    System.Console.WriteLine("Программистов");
---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите цифру обознчающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 6:
+    System.Console.WriteLine("Da"); 
+    break;
+    case 7:
+    System.Console.WriteLine("Da");
+    break;
+    case 1:
+    System.Console.WriteLine("Net");
+    break;
+    default:
+    System.Console.WriteLine("не верное значение");
+    break;
+}
