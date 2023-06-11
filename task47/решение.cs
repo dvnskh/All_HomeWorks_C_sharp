87276f29f8e9d966070f7c66406668f94725c617
Artem <dvinskikh_artem@mail.ru>
2023-06-11 17:49:46 +0900

    solution

--- task47/решение.cs
+++ task47/решение.cs
@@ -1,32 +1,29 @@
-﻿// задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
-Console.WriteLine("Задайте количество строк двумерного массива:");
+﻿Console.WriteLine("Задайте количество строк двумерного массива:");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Задайте количество столбцов двумерного массива:");
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] Array = new double[m, n];
 Random rnd = new Random();
 void PrintArray(double[,] array)
+{ for (int i = 0; i < m; i++)
+ { 
+   for (int j = 0; j < n; j++)
 { 
-    for (int i = 0; i < m; i++)
- { 
-    for (int j = 0; j < n; j++)
- { 
-    Console.Write(array [i, j]+"\t");
+   Console.Write(array[i, j] + "\t");
 }
- Console.WriteLine();
+   Console.WriteLine();
  }
 }
 
 void FillArray(double[,] array)
 { for (int i = 0; i < m; i++)
-    { for (int j = 0; j < n; j++)
  { 
-    array[i,j] = Convert.ToDouble(rnd.Next(-1000, 1000)/10.0);
+   for (int j = 0; j < n; j++)
+ { 
+   array[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);
  }
-    }
+ }
 }
-
 FillArray(Array);
 Console.WriteLine();
 PrintArray(Array);
---
+++ task50/решение.cs
@@ -0,0 +1,77 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+//  и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+static void FillArray(int[,] array)
+    {
+        Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = random.Next(0, 10);
+            }
+        }
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j],3}    ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void DoubleNum(int[,] array)
+    {
+        Console.WriteLine("Введите проверочное число: ");
+        int revesor = Convert.ToInt32(Console.ReadLine());
+        bool numberFound = false;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] == revesor)
+                {
+                    Console.WriteLine("Такое число есть");
+                    numberFound = true;
+                    break;
+                }
+            }
+
+            if (numberFound)
+            {
+                break;
+            }
+        }
+
+        if (!numberFound)
+        {
+            Console.WriteLine("Такого числа нет");
+        }
+    }
+
+
+    
+        Console.Clear();
+        Console.WriteLine("Введите количество строк:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество столбцов:");
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[rows, cols];
+
+        FillArray(array);
+        PrintArray(array);
+        Console.WriteLine();
+        DoubleNum(array);
+        PrintArray(array);
+    
